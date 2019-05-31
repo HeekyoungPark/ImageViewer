debe2b159809cfd3f8e778214082ab0ad362aa27
Heekyoung Park <acdong0202@gmail.com>
2019-05-31 11:27:22 +0900

    #1 사진첩 폼 디자인

--- ImageViewer/Form1.Designer.cs
+++ ImageViewer/Form1.Designer.cs
@@ -29,12 +29,125 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picImg = new System.Windows.Forms.PictureBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImg
+            // 
+            this.picImg.Location = new System.Drawing.Point(10, 10);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(256, 144);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImg.TabIndex = 0;
+            this.picImg.TabStop = false;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(10, 160);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(84, 46);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "이전";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(182, 160);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(84, 46);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "다음";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // lblNum
+            // 
+            this.lblNum.Font = new System.Drawing.Font("굴림", 14F);
+            this.lblNum.Location = new System.Drawing.Point(120, 160);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(36, 46);
+            this.lblNum.TabIndex = 2;
+            this.lblNum.Text = "/";
+            this.lblNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "43.JPG");
+            this.imgList.Images.SetKeyName(1, "1.jpg");
+            this.imgList.Images.SetKeyName(2, "2.JPG");
+            this.imgList.Images.SetKeyName(3, "3.JPG");
+            this.imgList.Images.SetKeyName(4, "4.JPG");
+            this.imgList.Images.SetKeyName(5, "5.JPG");
+            this.imgList.Images.SetKeyName(6, "6.JPG");
+            this.imgList.Images.SetKeyName(7, "7.JPG");
+            this.imgList.Images.SetKeyName(8, "8.jpg");
+            this.imgList.Images.SetKeyName(9, "9.JPG");
+            this.imgList.Images.SetKeyName(10, "10.JPG");
+            this.imgList.Images.SetKeyName(11, "11.JPG");
+            this.imgList.Images.SetKeyName(12, "12.JPG");
+            this.imgList.Images.SetKeyName(13, "13.JPG");
+            this.imgList.Images.SetKeyName(14, "14.JPG");
+            this.imgList.Images.SetKeyName(15, "15.JPG");
+            this.imgList.Images.SetKeyName(16, "16.JPG");
+            this.imgList.Images.SetKeyName(17, "17.JPG");
+            this.imgList.Images.SetKeyName(18, "18.JPG");
+            this.imgList.Images.SetKeyName(19, "19.JPG");
+            this.imgList.Images.SetKeyName(20, "20.JPG");
+            this.imgList.Images.SetKeyName(21, "21.JPG");
+            this.imgList.Images.SetKeyName(22, "22.JPG");
+            this.imgList.Images.SetKeyName(23, "23.JPG");
+            this.imgList.Images.SetKeyName(24, "24.JPG");
+            this.imgList.Images.SetKeyName(25, "25.JPG");
+            this.imgList.Images.SetKeyName(26, "26.JPG");
+            this.imgList.Images.SetKeyName(27, "27.JPG");
+            this.imgList.Images.SetKeyName(28, "28.JPG");
+            this.imgList.Images.SetKeyName(29, "29.JPG");
+            this.imgList.Images.SetKeyName(30, "30.JPG");
+            this.imgList.Images.SetKeyName(31, "31.JPG");
+            this.imgList.Images.SetKeyName(32, "32.JPG");
+            this.imgList.Images.SetKeyName(33, "33.JPG");
+            this.imgList.Images.SetKeyName(34, "34.JPG");
+            this.imgList.Images.SetKeyName(35, "35.JPG");
+            this.imgList.Images.SetKeyName(36, "36.JPG");
+            this.imgList.Images.SetKeyName(37, "37.JPG");
+            this.imgList.Images.SetKeyName(38, "38.JPG");
+            this.imgList.Images.SetKeyName(39, "39.JPG");
+            this.imgList.Images.SetKeyName(40, "40.JPG");
+            this.imgList.Images.SetKeyName(41, "41.JPG");
+            this.imgList.Images.SetKeyName(42, "42.JPG");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(277, 218);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.picImg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "사진첩";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picImg;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.ImageList imgList;
     }
 }
 
